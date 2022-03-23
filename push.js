833e48f52dd85c780c0c50777396d5c63a70598d
KarlD <kdivina@viribuz.legal>
2022-03-24 01:49:41 +0800

    fix

--- push.js
+++ push.js
@@ -7,7 +7,7 @@ let vapidkeys = {
 
 push.setVapidDetails('mailto:tite', vapidkeys.publicKey, vapidkeys.privateKey)
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dr8J3OYbA6w:APA91bGX1ZuQjvvmd6IZo_U82HwaDn6T8W9scvNtbj4tUqXOH0uKPIUqRGFUjaeVg7Z-IKqM0gelFILUhXglgbPTRAQ99ihkCBc9Y0IdrUFKl2yChJPStOSf0Cbur_n5PBU1MKJQuRA3","expirationTime":null,"keys":{"p256dh":"BEPkwE0lONIjaFpgtibEIR-kP0IlsHjVv37NpY9z7HVwHErPg-tlAecyiwArgaUhP0TFX3qNGaa2nLOjfS2AZr4","auth":"CtdwqWHWmnKr1RSZwRkadQ"}};
 
 push.sendNotification(sub, 'test')
 
